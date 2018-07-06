d25f2b7ce1709239a3247efd21709b2e6fbcfae9
kchang4 <khai.chang89@gmail.com>
2018-07-06 17:34:01 -0500

    add more test coverage

--- src/divide.js
+++ src/divide.js
@@ -1,4 +1,10 @@
 function divide(a, b) {
+  if (isNaN(a)) {
+    throw new Error("a must be a number");
+  } else if (isNaN(b)) {
+    throw new Error("b must be a number");
+  }
+
   return a / b;
 }
 
--- src/divide.test.js
+++ src/divide.test.js
@@ -3,3 +3,15 @@ const divide = require("./divide");
 test("divides 6 / 3 to equal 2", function () {
   expect(divide(6, 3)).toBe(2);
 });
+
+test("divides a / 3 to throw an error", function () {
+  expect(() => {
+    divide("a", 3);
+  }).toThrowError("a must be a number");
+});
+
+test("divides 6 / b to throw an error", function () {
+  expect(() => {
+    divide(6, "b");
+  }).toThrowError("b must be a number");
+});
